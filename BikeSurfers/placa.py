22bbcd598c462893845b3cec7d359686b593c17d
Rildian <rildian.nbeserra@gmail.com>
2025-02-20 18:54:24 -0300

    [refactor]placa agora é um unico objeto

--- BikeSurfers/placa.py
+++ BikeSurfers/placa.py
@@ -1,83 +1,102 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from texture import Texture  # Certifique-se de importar a classe Texture
+from texture import Texture
 import math
-from poste_placa import Poste_placa
 
 class Placa:
-    def __init__(self, texture_path="textura.png", initial_position=[0.0, 0.0, 0.0]):
+    def __init__(self, texture_path="Pare.jpg", initial_position=[0.0, 0.0, 0.0]):
+        # Configurações da placa
         self.position = initial_position.copy()
         self.valor = [0.0, 0.0, 0.0]
         self.angulo = 0.0
         self.texture = Texture(texture_path)
+        
+        # Geometria da placa
         self.vertices = self.calcular_vertices()
         self.tex_coords = self.calcular_tex_coords()
-        self.peca = Poste_placa()
-
-    def calcular_vertices(self):
-        """Calcula os vértices do octógono."""
-        raio = 0.5
-        vertices = []
         
-        # Calcula os vértices do octógono
+        # Configurações do poste
+        self.poste_position = [0.6, -3.2, 0.0]  # Offset em relação à placa
+        self.poste_scale = [0.1, 1.5, 0.1]
+        
+        # Dados geométricos do poste (cubo)
+        self.poste_vertices = [
+            [-0.5, -0.5, -0.5], [0.5, -0.5, -0.5], [0.5, 0.5, -0.5], [-0.5, 0.5, -0.5],
+            [-0.5, -0.5, 0.5], [0.5, -0.5, 0.5], [0.5, 0.5, 0.5], [-0.5, 0.5, 0.5]
+        ]
+        self.poste_faces = [
+            [0, 1, 2, 3], [4, 5, 6, 7], [0, 1, 5, 4],
+            [2, 3, 7, 6], [0, 3, 7, 4], [1, 2, 6, 5]
+        ]
+
+    def calcular_vertices(self, raio=0.5):
+        vertices = []
         for i in range(8):
-            angulo = math.radians(i * (360/8))  # 360° / 8 lados = 45° por vértice
+            angulo = math.radians(i * 45)
             x = raio * math.cos(angulo)
             y = raio * math.sin(angulo)
-            vertices.append([x, y, 0.0])  # Adiciona o vértice ao octógono
-        
+            vertices.append([x, y, 0.0])
         return vertices
 
     def calcular_tex_coords(self):
-        """Calcula as coordenadas de textura para o octógono."""
-        # Coordenadas de textura para um octógono (mapeamento UV)
         return [
-            (0.1,0.66), (0.33,0.9), (0.66,0.9), (0.9, 0.66), (0.9, 0.33), (0.66,0.1), (0.33, 0.1), (0.1, 0.33)
+            (0.1,0.66), (0.33,0.9), (0.66,0.9), (0.9, 0.66),
+            (0.9, 0.33), (0.66,0.1), (0.33, 0.1), (0.1, 0.33)
         ]
-
-
 
     def mover(self, x: float, y: float, z: float):
         self.valor[0] += x
         self.valor[1] += y
         self.valor[2] += z
 
+    def set_posicao(self, x, y, z):
+        self.valor = [x, y, z]
+
     def update(self, velocidade):
         self.valor[0] -= velocidade
 
-    def draw_peca(self, x, y, z, a, b, c):
-        self.peca.draw(x, y, z, a, b, c)
+    def draw(self, x, y, z, tamanho):
+        self._draw_placa(x, y, z, tamanho)
+        self._draw_poste(x, y, z)
 
-    def set_posicao(self, x, y, z):
-        self.valor[0] = x
-        self.valor[1] = y
-        self.valor[2] = z
-
-    def draw_placa(self, x,y,z,tamanho):
-        glColor3f(1.0, 1.0, 1.0)  # Define a cor branca (a textura vai sobrescrever)
+    def _draw_placa(self, x, y, z, tamanho):
         glPushMatrix()
-        glTranslatef(self.position[0] + self.valor[0] + x, self.position[1] + self.valor[1] + y,self.position[2] + self.valor[2] + z)
-        glRotate(90,0, 1, 0)
+        glTranslatef(
+            self.position[0] + self.valor[0] + x,
+            self.position[1] + self.valor[1] + y,
+            self.position[2] + self.valor[2] + z
+        )
+        glRotate(90, 0, 1, 0)
         glScale(tamanho/1.5, tamanho/1.5, 1/1.5)
-        glRotate(self.angulo, 0, 0, 1) 
-        glRotate(22.5, 0, 0, 1) 
+        glRotate(self.angulo, 0, 0, 1)
+        glRotate(22.5, 0, 0, 1)
 
-        self.texture.bind()  # Ativa a textura
+        self.texture.bind()
         glEnable(GL_TEXTURE_2D)
-
+        
         glBegin(GL_POLYGON)
         for i, vertice in enumerate(self.vertices):
-            # Aplica a coordenada de textura para cada vértice
-            glTexCoord2f(*self.tex_coords[i])  # Mapeia a coordenada UV
-            glVertex3f(*vertice)  # Define a posição do vértice
+            glTexCoord2f(*self.tex_coords[i])
+            glVertex3f(*vertice)
         glEnd()
+        
         glDisable(GL_TEXTURE_2D)
-        self.texture.unbind()  # Desativa a textura
-
+        self.texture.unbind()
         glPopMatrix()
 
-    def draw(self, x, y, z, tamanho):
-        self.draw_placa(x,y+2,z,tamanho)
-        self.draw_peca(self.position[0] + self.valor[0] + x + 0.6, self.position[1] + self.valor[1] + y - 3.2, self.position[2] + self.valor[2] + z, tamanho * 0.1, tamanho * 1.5, tamanho * 0.1)
+    def _draw_poste(self, x, y, z):
+        glPushMatrix()
+        glTranslatef(
+            self.position[0] + self.valor[0] + x + self.poste_position[0],
+            self.position[1] + self.valor[1] + y + self.poste_position[1],
+            self.position[2] + self.valor[2] + z + self.poste_position[2]
+        )
+        glScale(*self.poste_scale)
+        glColor3f(0.3, 0.3, 0.3)
         
-
+        glBegin(GL_QUADS)
+        for face in self.poste_faces:
+            for vertex in face:
+                glVertex3fv(self.poste_vertices[vertex])
+        glEnd()
+        glPopMatrix()
